c3f52313f1ea28ffa3bc7326be2f3614ec5167a0
Halton Bagley <haltonba94@gmail.com>
2019-12-18 22:22:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(musician, instrument) {
+  let band = [];
+  for (let i = 0; i < musician.length; i++) {
+    band.push(`${musician[i]} plays ${instrument[i]}`);
+  }
+  return band;
+}
+
+function johnLennonFacts (facts) {
+  let count = 0;
+  while (count < facts.length) {
+    facts[count] += "!!!";
+    count++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles (number) {
+  let ilb = [];
+  let count = 15 - number;
+  do {
+    ilb.push("I love the Beatles");
+    count++;
+  }
+  while (count < 15);
+  return ilb;
+}
